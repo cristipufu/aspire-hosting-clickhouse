f26916cf8b000530e51f93a7e49b08f18b10f8c1
dogacans <dogacans@gmail.com>
2025-07-28 21:36:51 +0300

    Testing "latest" image tag on the playground

--- playground/ClickHouse.AppHost/Program.cs
+++ playground/ClickHouse.AppHost/Program.cs
@@ -1,6 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var clickhouse = builder.AddClickHouse("clickhouse")
+var clickhouse = builder.AddClickHouse("clickhouse", imageTag: "latest")
                     .AddDatabase("default");
 
 builder.AddProject<Projects.ClickHouse_DemoApi>("api")
